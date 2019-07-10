a928767c835a3b63d1238a7f4ee73a78dba0d228
Kujo4pm <kujo4pm@gmail.com>
2019-07-10 20:34:18 +1000

    Getting bounds of window

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,7 +3,10 @@ import ReactMapGL from 'react-map-gl';
 
 const { REACT_APP_MAPBOX_API_TOKEN: MAPBOX_API_TOKEN } = process.env;
 class Map extends Component {
-
+    constructor(props) {
+        super(props);
+        this.mapRef= React.createRef();
+    }
   state = {
     viewport: {
       width: 800,
@@ -13,13 +16,29 @@ class Map extends Component {
       zoom: 12
     }
   };
+  getMapBoundaries = () => {
+    if (!this.mapRef.getMap) return;
+    // Get map boundaries
+    const myMap = this.mapRef.getMap(); 
+    return myMap.getBounds();
+  }
 
+  updateViewport = (viewport) => {
+    const mapBounds = this.getMapBoundaries();
+    this.setState({ viewport, mapBounds });
+  };
+
+  componentDidMount = () => {
+    const mapBounds = this.getMapBoundaries();
+    this.setState({ mapBounds });
+  };
   render() {
     return (
       <ReactMapGL
         mapboxApiAccessToken={MAPBOX_API_TOKEN}
         {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}
+        onViewportChange={this.updateViewport}
+        ref={map => this.mapRef = map}
       />
     );
   }
