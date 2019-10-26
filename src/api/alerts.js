7034cca35ecdd99c9c6fda872356aef9c8111dbf
Kurt Johnson <kurt@beyondessential.com.au>
2019-10-26 13:17:49 +1100

    Moving passthrough

--- src/api/alerts.js
+++ src/api/alerts.js
@@ -1,21 +1,11 @@
-import axios from 'axios';
-const {
-  REACT_APP_OPEN_PLANNING_API_TOKEN: OPEN_PLANNING_API_TOKEN
-} = process.env;
-const BASE_URL =
-  'https://cors-anywhere.herokuapp.com/https://api.planningalerts.org.au/applications.js';
+import { fetch, API_LIST } from '.';
 const fetchAlerts = ({ bottomLeft, topRight }) => {
   console.log(`fetching for ${(bottomLeft, topRight)} `);
-  return axios({
-    method: 'get',
-    url: BASE_URL,
-    params: {
-      key: OPEN_PLANNING_API_TOKEN,
-      bottom_left_lat: bottomLeft.lat,
-      bottom_left_lng: bottomLeft.lng,
-      top_right_lat: topRight.lat,
-      top_right_lng: topRight.lng
-    }
+  return fetch(API_LIST.OPEN_PLANNING, {
+    bottom_left_lat: bottomLeft.lat,
+    bottom_left_lng: bottomLeft.lng,
+    top_right_lat: topRight.lat,
+    top_right_lng: topRight.lng
   });
 };
 
